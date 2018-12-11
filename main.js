b840a7f60eecfd823f4e6ff73b2f3300d8639490
Jamie Strausbaugh <jstrausb86@gmail.com>
2018-12-10 19:16:10 -0500

    Added methods to make balls bounce off the sides of the viewport as well as generate the balls and run the animation loop

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-
 // setup canvas
 
 const canvas = document.querySelector('canvas');
@@ -24,10 +23,65 @@ function Ball(x, y, velX, velY, color, size) {
   this.size = size;
 }
 
-// Adding the draw method to the Ball's prototype
-Ball.prototype.draw = () => {
+// adding the draw method to the Ball's prototype
+Ball.prototype.draw = function() {
   ctx.beginPath();
   ctx.fillStyle = this.color;
   ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
   ctx.fill();
 }
+
+// updating the ball's data
+Ball.prototype.update = function() {
+  if ((this.x + this.size) >= width) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.x - this.size) <= 0) {
+    this.velX = -(this.velX);
+  }
+
+  if ((this.y + this.size) >= height) {
+    this.velY = -(this.velY);
+  }
+
+  if ((this.y - this.size) <= 0) {
+    this.velY = -(this.velY);
+  }
+
+  this.x += this.velX;
+  this.y += this.velY;
+}
+
+// balls container
+const balls = [];
+
+// loop to generate the balls, draw and update their position on the page
+function loop () {
+  ctx.fillStyle = 'rgba(0, 0, 0, 0.25';
+  ctx.fillRect(0, 0, width, height);
+
+  while (balls.length < 25) {
+    let size = random(10, 20);
+    let ball = new Ball(
+      // ball position always drawn at least one ball width
+      // away from the edge of the canvas to avoid drawing errors
+      random(0 + size, width - size),
+      random(0 + size, width - size),
+      random(-7, 7),
+      random(-7, 7),
+      'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')',
+      size
+    );
+    balls.push(ball);
+  }
+
+  for (let i = 0; i < balls.length; i++) {
+    balls[i].draw();
+    balls[i].update();
+  }
+
+  requestAnimationFrame(loop);
+}
+
+loop();
